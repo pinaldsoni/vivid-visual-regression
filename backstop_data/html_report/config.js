9007be173333815b6a48233f0ddf0bd4f9c12dcf
baumannalexj <baumannalexj@gmail.com>
2018-03-14 15:12:50 -0500

    no package json or node

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,92 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_nav_bar_-_help_0_document_0_phone.png",
+        "test": "../bitmaps_test/20180314-121935/backstop_default_nav_bar_-_help_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_nav_bar_-_help_0_document_0_phone.png",
+        "label": "nav bar - help",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_nav_bar_-_help_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20180314-121935/backstop_default_nav_bar_-_help_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_nav_bar_-_help_0_document_1_tablet.png",
+        "label": "nav bar - help",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "1.13",
+          "analysisTime": 75
+        },
+        "diffImage": "../bitmaps_test/20180314-121935/failed_diff_backstop_default_nav_bar_-_help_0_document_1_tablet.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_production_header_-_buyer_guarantee_0_document_0_phone.png",
+        "test": "../bitmaps_test/20180314-121935/backstop_default_production_header_-_buyer_guarantee_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_production_header_-_buyer_guarantee_0_document_0_phone.png",
+        "label": "production header - buyer guarantee",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.54",
+          "analysisTime": 43
+        },
+        "diffImage": "../bitmaps_test/20180314-121935/failed_diff_backstop_default_production_header_-_buyer_guarantee_0_document_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_production_header_-_buyer_guarantee_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20180314-121935/backstop_default_production_header_-_buyer_guarantee_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_production_header_-_buyer_guarantee_0_document_1_tablet.png",
+        "label": "production header - buyer guarantee",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.72",
+          "analysisTime": 68
+        },
+        "diffImage": "../bitmaps_test/20180314-121935/failed_diff_backstop_default_production_header_-_buyer_guarantee_0_document_1_tablet.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "backstop_default"
+});
